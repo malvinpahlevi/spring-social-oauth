383f75ecb9fd4f6dc7caedd4b6faca3d1ed15d09
malvinpahlevi <56686418+malvinpahlevi@users.noreply.github.com>
2020-09-13 22:25:23 +0700

    completed configure User.java also getters and setters

--- src/main/java/com/yanmalvin/springsocialoauth/model/User.java
+++ src/main/java/com/yanmalvin/springsocialoauth/model/User.java
@@ -1,7 +1,11 @@
 package com.yanmalvin.springsocialoauth.model;
 
+import net.minidev.json.annotate.JsonIgnore;
+
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.security.AuthProvider;
 
 @Entity
 @Table(name = "users", uniqueConstraints = {
@@ -17,6 +21,84 @@ public class User {
     private String name;
 
     @Email
+    @Column(nullable = false)
+    private String email;
 
+    private String imageUrl;
 
+    @Column(nullable = false)
+    private Boolean emailVerified = false;
+
+    @JsonIgnore
+    private  String password;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private AuthProvider provider;
+
+    private String providerId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Boolean getEmailVerified() {
+        return emailVerified;
+    }
+
+    public void setEmailVerified(Boolean emailVerified) {
+        this.emailVerified = emailVerified;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public AuthProvider getProvider() {
+        return provider;
+    }
+
+    public void setProvider(AuthProvider provider) {
+        this.provider = provider;
+    }
+
+    public String getProviderId() {
+        return providerId;
+    }
+
+    public void setProviderId(String providerId) {
+        this.providerId = providerId;
+    }
 }
