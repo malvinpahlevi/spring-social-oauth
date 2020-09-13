502857568f47f08b04f22ae759a224c3bc863c1b
malvinpahlevi <56686418+malvinpahlevi@users.noreply.github.com>
2020-09-13 22:21:46 +0700

    add javax validation dependency in pom.xml

--- src/main/java/com/yanmalvin/springsocialoauth/model/User.java
+++ src/main/java/com/yanmalvin/springsocialoauth/model/User.java
@@ -1,12 +1,22 @@
 package com.yanmalvin.springsocialoauth.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
 
 @Entity
 @Table(name = "users", uniqueConstraints = {
         @UniqueConstraint(columnNames = "email")
 })
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Email
+
+
 }
