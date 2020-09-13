93c5f9b6addfa2d6484177c8939d0e462673e44e
malvinpahlevi <56686418+malvinpahlevi@users.noreply.github.com>
2020-09-13 20:53:30 +0700

    first project

---
+++ src/main/java/com/yanmalvin/springsocialoauth/SpringSocialOauthApplication.java
@@ -0,0 +1,13 @@
+package com.yanmalvin.springsocialoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocialOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSocialOauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yanmalvin/springsocialoauth/SpringSocialOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanmalvin.springsocialoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSocialOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
